045e528ae4906b98c0c1da77de633dfa58ede023
linmt <549343315@qq.com>
2018-10-28 23:52:32 +0800

    181028  搭建mybatis环境

---
+++ src/main/java/com/lmt/entity/Dept.java
@@ -0,0 +1,34 @@
+package com.lmt.entity;
+
+/**
+ * Created by 热带雨林 on 2018/10/28.
+ */
+public class Dept {
+    private Integer deptno;
+    private String dname;
+    private String loc;
+
+    public Integer getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(Integer deptno) {
+        this.deptno = deptno;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
+}
---
+++ src/main/java/com/lmt/entity/DeptMapper.java
@@ -0,0 +1,17 @@
+package com.lmt.entity;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by 热带雨林 on 2018/10/28.
+ */
+public interface DeptMapper {
+    void addDept(Dept dept);
+    void deleteDept(Dept dept);
+    void updateDept(Dept dept);
+    Dept findDeptById(Integer deptno);
+    List<Dept> findAllDept();
+    List<Dept> findDeptByLoc(String loc);
+    List<Map<String, Object>> findAllDname(String loc);
+}
